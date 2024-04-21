bc0e7088e8871c2013abe20e7df74cae3fa4ac1b
Eric <ericfrancis1@proton.me>
2024-04-21 09:10:15 -0500

    connectWithRetry()

--- server/db/index.js
+++ server/db/index.js
@@ -2,9 +2,15 @@ const mongoose = require('mongoose');
 
 const connectionString = 'mongodb://mongo:27017/cinema';
 
-mongoose.connect(connectionString, { useNewUrlParser: true }).catch((e) => {
-  console.error('Connection error', e.message);
-});
+const connectWithRetry = () => {
+  mongoose.connect(connectionString, { useNewUrlParser: true })
+    .then(() => console.log('Connected to DB'))
+    .catch(e => {
+      console.error('Connection error', e.message);
+      setTimeout(connectWithRetry, 5000);
+    });
+}
+connectWithRetry();
 
 const db = mongoose.connection;
 
